57cbfae544dd2eaae2b27537be1e963171372857
Felipe Morilho <felipemdecastro@gmail.com>
2023-12-29 10:57:24 -0300

    feat: add implementation of the code challenge

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,57 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(var nome: String, var email: String)
 
-class Usuario
+data class ConteudoEducacional(var nome: String, val duracao: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
+    
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        
+        println("${usuario.nome} foi inscrito no curso $nome")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val javaBasico = ConteudoEducacional("Java ${Nivel.BASICO}", 100)
+    val javaAvancado = ConteudoEducacional("Java ${Nivel.AVANCADO}", 60)
+    val javaIntermediario = ConteudoEducacional("Java ${Nivel.INTERMEDIARIO}", 45)
+    
+    val kotlinBasico = ConteudoEducacional("Kotlin ${Nivel.BASICO}", 75)
+    val kotlinAvancado = ConteudoEducacional("Kotlin ${Nivel.AVANCADO}", 60)
+    val androidBasico = ConteudoEducacional("Android ${Nivel.BASICO}", 90)  
+    
+    val conteudos = listOf(javaBasico, javaAvancado, javaIntermediario, kotlinBasico, kotlinAvancado, androidBasico)
+        
+    val formacaoKotlin = Formacao("Kotlin Fundamentals", listOf(kotlinBasico, kotlinAvancado, androidBasico), nivel = Nivel.INTERMEDIARIO)
+    val formacaoJava = Formacao("Java Developer", listOf(javaBasico, javaIntermediario, javaAvancado, androidBasico), nivel = Nivel.AVANCADO)
+    
+    val formacoes = listOf(formacaoKotlin, formacaoJava)
+
+    val usuarioUm = Usuario("João Figueiredo", "jojo@gmail.com")
+    val usuarioDois = Usuario("Renata Silveira", "resilveira@gmail.com")
+    val usuarioTres = Usuario("Lucas Costa", "lucosta@hotmail.com")
+    val usuarioQuatro = Usuario("Gilmar Gili", "gili@gmail.com")
+
+    formacaoKotlin.matricular(usuarioUm)
+    formacaoKotlin.matricular(usuarioDois)
+    formacaoJava.matricular(usuarioTres)
+    formacaoJava.matricular(usuarioQuatro)
+    formacaoKotlin.matricular(usuarioQuatro)
+
+    println("\nFormações disponíveis: ${formacoes.map { it.nome }}\n")
+    for (formacao in formacoes){
+        println("Formação: ${formacao.nome} - Nível: ${formacao.nivel}")
+        println("Inscritos na ${formacao.nome}: ${formacao.inscritos.map { it.nome }}")
+    }
+    
+    println("\nConteúdos para estudo: ")
+    for(conteudo in conteudos){
+        println("Curso: ${conteudo.nome} - Carga Horária: ${conteudo.duracao}h")
+    }
+
 }
